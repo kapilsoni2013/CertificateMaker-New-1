dabf971ec269ad9098df74dec8a3723c41ec8705
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-12 14:11:32 +0530

    Update App.js to add dynamic form builder module

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Sidebar from './components/shared/Sidebar';
 import StudentRecords from './components/StudentRecords';
 import TemplateManager from './components/TemplateManager';
 import CertificateGenerator from './components/CertificateGenerator';
+import FormBuilder from './components/FormBuilder';
+import CertificatePreview from './components/CertificatePreview';
 import NotFound from './components/shared/NotFound';
 
 function App() {
@@ -22,6 +24,8 @@ function App() {
             <Route path="/students" element={<StudentRecords />} />
             <Route path="/templates" element={<TemplateManager />} />
             <Route path="/generate" element={<CertificateGenerator />} />
+            <Route path="/form-builder" element={<FormBuilder />} />
+            <Route path="/certificate-preview/:id" element={<CertificatePreview />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
