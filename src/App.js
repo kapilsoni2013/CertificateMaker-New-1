644c220c2891e62a0fe35153166b6d653638a02e
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-12 16:39:30 +0530

    Remove duplicate Router and AppProvider in App.js

--- src/App.js
+++ src/App.js
@@ -8,38 +8,35 @@ import TemplateManager from './components/TemplateManager';
 import CertificateGenerator from './components/CertificateGenerator';
 import FormBuilder from './components/FormBuilder';
 import CertificatePreview from './components/CertificatePreview';
-import { AppProvider } from './context/AppContext';
 import Header from './components/shared/Header';
 import Footer from './components/shared/Footer';
 
 function App() {
   return (
-    <AppProvider>
-      <div className="app-container">
-        <Header />
-        <div className="main-content">
-          <nav className="main-nav">
-            <ul>
-              <li><Link to="/">Student Records</Link></li>
-              <li><Link to="/templates">Template Manager</Link></li>
-              <li><Link to="/generator">Certificate Generator</Link></li>
-              <li><Link to="/form-builder">Form Builder</Link></li>
-              <li><Link to="/preview">Certificate Preview</Link></li>
-            </ul>
-          </nav>
-          <div className="content-area">
-            <Routes>
-              <Route path="/" element={<StudentRecords />} />
-              <Route path="/templates" element={<TemplateManager />} />
-              <Route path="/generator" element={<CertificateGenerator />} />
-              <Route path="/form-builder" element={<FormBuilder />} />
-              <Route path="/preview" element={<CertificatePreview />} />
-            </Routes>
-          </div>
+    <div className="app-container">
+      <Header />
+      <div className="main-content">
+        <nav className="main-nav">
+          <ul>
+            <li><Link to="/">Student Records</Link></li>
+            <li><Link to="/templates">Template Manager</Link></li>
+            <li><Link to="/generator">Certificate Generator</Link></li>
+            <li><Link to="/form-builder">Form Builder</Link></li>
+            <li><Link to="/preview">Certificate Preview</Link></li>
+          </ul>
+        </nav>
+        <div className="content-area">
+          <Routes>
+            <Route path="/" element={<StudentRecords />} />
+            <Route path="/templates" element={<TemplateManager />} />
+            <Route path="/generator" element={<CertificateGenerator />} />
+            <Route path="/form-builder" element={<FormBuilder />} />
+            <Route path="/preview" element={<CertificatePreview />} />
+          </Routes>
         </div>
-        <Footer />
       </div>
-    </AppProvider>
+      <Footer />
+    </div>
   );
 }
 
