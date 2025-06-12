a38ac7cb9b52064f1c43c069f7a2b1e3189ac4ca
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-12 15:41:17 +0530

    Fix AppContext export issue

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -146,7 +146,7 @@ const appReducer = (state, action) => {
 };
 
 // Create context
-export const AppContext = createContext();
+const AppContext = createContext();
 
 // Provider component
 export const AppProvider = ({ children }) => {
@@ -451,4 +451,8 @@ export const useAppContext = () => {
   return context;
 };
 
+// Export the context directly along with the provider and hook
+export { AppContext };
+
+// Export AppContext as default export as well
 export default AppContext;
