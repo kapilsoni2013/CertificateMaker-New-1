b8c95af690598d082f0c38737ed1bccc0f31225c
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-12 15:10:44 +0530

    Update FormBuilder component

--- src/components/FormBuilder/index.js
+++ src/components/FormBuilder/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import DynamicFormBuilder from './DynamicFormBuilder';
 import './FormBuilder.css';
 
 const FormBuilder = () => {
   return (
     <div className="form-builder-container">
       <h2>Form Builder</h2>
-      <p>This component will be implemented soon.</p>
+      <DynamicFormBuilder />
     </div>
   );
 };
