743677d0ccef709d1f2944c674d99798f417fb61
Kapil Sony A White Hat Hacker <kapilsoni2013@gmail.com>
2025-06-12 14:34:00 +0530

    Add FormBuilder component

---
+++ src/components/FormBuilder/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './FormBuilder.css';
+
+const FormBuilder = () => {
+  return (
+    <div className="form-builder-container">
+      <h2>Form Builder</h2>
+      <p>This component will be implemented soon.</p>
+    </div>
+  );
+};
+
+export default FormBuilder;
